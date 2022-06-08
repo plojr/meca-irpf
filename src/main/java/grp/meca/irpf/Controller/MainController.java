9f55e603426fe43dc2f8f0dcfbbf4a3ed7a7ea29
paulo <paulo@paulo>
2022-06-07 22:30:31 -0300

    Implementação do método mostrarCorretagens

--- src/main/java/grp/meca/irpf/Controller/MainController.java
+++ src/main/java/grp/meca/irpf/Controller/MainController.java
@@ -1,10 +1,24 @@
 package grp.meca.irpf.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import grp.meca.irpf.Models.NotaDeCorretagem;
+import grp.meca.irpf.Repositories.NotaDeCorretagemRepository;
+import grp.meca.irpf.Repositories.OrdemRepository;
 
 @Controller
 public class MainController {
+	
+	@Autowired
+	private OrdemRepository ordemRepository;
+	
+	@Autowired
+	private NotaDeCorretagemRepository corretagemRepository;
 
 	@GetMapping("/")
 	public String home() {
@@ -12,7 +26,13 @@ public class MainController {
 	}
 	
 	@GetMapping("/corretagens")
-	public String mostrarCorretagens() {
+	public String mostrarCorretagens(Model model) {
+		List<NotaDeCorretagem> corretagens = corretagemRepository.findAll();
+		for(NotaDeCorretagem nc: corretagens)
+			nc.setOrdens(ordemRepository.findByNotaDeCorretagem(nc));
+		model.addAttribute("corretagens", corretagens);
 		return "corretagens";
 	}
+	
+	
 }
