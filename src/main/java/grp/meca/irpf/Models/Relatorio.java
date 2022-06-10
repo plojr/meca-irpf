cb94f7a21baf0ffec30d98f0cf8cdd1aeb70ac69
paulo <paulo@paulo>
2022-06-09 22:26:25 -0300

    Criada a classe para os relatórios

---
+++ src/main/java/grp/meca/irpf/Models/Relatorio.java
@@ -0,0 +1,28 @@
+package grp.meca.irpf.Models;
+
+import java.util.List;
+import java.util.Map;
+
+import lombok.Data;
+
+@Data
+public class Relatorio {
+
+	private int id;
+	
+	private List<ItemCarteira> carteira;
+	// Estes Map<String, Double> são mapeamentos de mês -> lucro/prejuízo.
+	private Map<String, Double> swingTradeIsento;
+	private Map<String, Double> swingTradeTributavel;
+	private Map<String, Double> mensalDayTrade;
+	private double prejuizoAcumuladoSwingTrade;
+	private double prejuizoAcumuladoDayTrade;
+	
+	// TODO
+	public double getSomaLucroSwingTradeIsento() {
+		double lucro = 0;
+		
+		return lucro;
+	}
+	
+}
