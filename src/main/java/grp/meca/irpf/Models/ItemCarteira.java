194bf1bc1227015ad3654af7f881c05c7382a856
paulo <paulo@paulo>
2022-06-11 09:37:38 -0300

    Alterada o ItemCarteira para não ser uma Entity

--- src/main/java/grp/meca/irpf/Models/ItemCarteira.java
+++ src/main/java/grp/meca/irpf/Models/ItemCarteira.java
@@ -9,33 +9,12 @@
 
 package grp.meca.irpf.Models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.OneToOne;
-
 import lombok.Data;
 
 @Data
-@Entity
-(name = "items_carteira")
 public class ItemCarteira {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-	@JoinColumn(name = "ticker_id", nullable = false)
 	private Ticker ticker;
-	
-	@Column(nullable = false)
 	private int quantidade;
-	
-	@Column(nullable = false)
 	private double custoTotal;
 }
