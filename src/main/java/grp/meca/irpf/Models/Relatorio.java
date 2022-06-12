a225a6f7f3e5aa1b458f286d7af5f8f1a29b530d
paulo <paulo@paulo>
2022-06-12 01:24:52 -0300

    Melhorada a estrutura de dados da classe Relatorio

--- src/main/java/grp/meca/irpf/Models/Relatorio.java
+++ src/main/java/grp/meca/irpf/Models/Relatorio.java
@@ -3,14 +3,13 @@ package grp.meca.irpf.Models;
 import java.util.List;
 import java.util.Map;
 
+import grp.meca.irpf.Services.Carteira;
 import lombok.Data;
 
 @Data
 public class Relatorio {
 
-	private int id;
-	
-	private List<ItemCarteira> carteira;
+	private Carteira carteira;
 	// Estes Map<String, Double> são mapeamentos de mês -> lucro/prejuízo.
 	private Map<String, Double> swingTradeIsento;
 	private Map<String, Double> swingTradeTributavel;
@@ -19,8 +18,9 @@ public class Relatorio {
 	private double prejuizoAcumuladoDayTrade;
 	
 	// TODO
-	public Relatorio(List<NotaDeCorretagem> corretagens) {
-		
+	public Relatorio(List<NotaDeCorretagem> corretagens) throws Exception {
+		this.carteira = new Carteira();
+		this.carteira.getCarteira(corretagens);
 	}
 	
 	// TODO
@@ -29,5 +29,7 @@ public class Relatorio {
 		
 		return lucro;
 	}
+	
+	
 	
 }
