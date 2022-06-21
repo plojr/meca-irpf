273008c47661b68c7fb62eb339ef24600b1f2c6a
plojr <ufvpaulo@gmail.com>
2022-06-20 22:43:38 -0300

    Melhoria na classe MapUtil

--- src/main/java/grp/meca/irpf/Utils/MapUtil.java
+++ src/main/java/grp/meca/irpf/Utils/MapUtil.java
@@ -14,4 +14,17 @@ public class MapUtil {
 			map.put(firstKey, localMap);
 		}
 	}
+	
+	public static void add(Map<Integer, Map<Integer, Double>> map, int firstKey, int secondKey, double value) {
+		if(map.containsKey(firstKey)) {
+			if(map.get(firstKey).containsKey(secondKey))
+				value += map.get(firstKey).get(secondKey);
+			map.get(firstKey).put(secondKey, value);
+		}
+		else {
+			Map<Integer, Double> localMap = new HashMap<>();
+			localMap.put(secondKey, value);
+			map.put(firstKey, localMap);
+		}
+	}
 }
