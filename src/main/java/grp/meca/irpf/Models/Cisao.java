aef7edbd17f359375aae734b494fdc402f2f930f
plojr <ufvpaulo@gmail.com>
2022-06-24 19:54:02 -0300

    Adição de eventos extraordinários

---
+++ src/main/java/grp/meca/irpf/Models/Cisao.java
@@ -0,0 +1,38 @@
+/*
+ * Esta classe é para representar a cisão que pode acontecer por parte das empresas.
+ * Exemplo de cisão de 2021 foi a abertura do capital da Getnet a partir da cisão
+ * do Santander. A porcentagem cindida foi aproximadamente de 3,1422%, sendo que
+ * a cada ação do Santander, o investidor recebeu 0,25 da mesma classe de Getnet.
+ * Isto é, se o investidor tinha ON de Santander, ele recebeu ON de Getnet, e assim
+ * por diante.
+ * É também importante salvar a data da cisão no banco. No exemplo acima, a data da
+ * cisão foi 18/10/2021.
+ */
+
+package grp.meca.irpf.Models;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper=false)
+@Data
+@Entity(name = "cisao")
+public class Cisao extends EventoExtraordinario {
+	
+	// Esta é a empresa que foi cindida.
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "ticker_id_2", nullable = false)
+	private Ticker ticker2;
+	
+	@Column(nullable = false)
+	private double porcentagemCindida;
+	
+	@Column(nullable = false)
+	private double proporcao;
+}
---
+++ src/main/java/grp/meca/irpf/Models/EventoExtraordinario.java
@@ -0,0 +1,39 @@
+/*
+ * Esta classe será a classe base para qualquer evento extraordinário.
+ * Lista de eventos extraordinários: cisão, fusão, grupamento e desdobramento.
+ * Os campos em comum a todos eles são: o id, a data do evento e a empresa,
+ * representada pelo ticker, responsável pelo evento.
+ * Quando o evento envolver outra empresa, como nos casos de cisão e fusão,
+ * a variável responsável por ela estará na classe filha.
+ */
+
+package grp.meca.irpf.Models;
+
+import java.time.LocalDate;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.OneToOne;
+
+import lombok.Data;
+
+@Data
+@MappedSuperclass
+public abstract class EventoExtraordinario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "ticker_id_1", nullable = false)
+	private Ticker ticker1;
+	
+	@Column(nullable = false)
+	private LocalDate dataCisao;
+}
