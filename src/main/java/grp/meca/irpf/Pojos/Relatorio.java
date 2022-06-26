d9a602bb936189367af5d83fe2ac67db0be14b36
plojr <ufvpaulo@gmail.com>
2022-06-25 21:08:57 -0300

    Classe Relatorio adaptada para os eventos extraordinários

--- src/main/java/grp/meca/irpf/Pojos/Relatorio.java
+++ src/main/java/grp/meca/irpf/Pojos/Relatorio.java
@@ -2,6 +2,7 @@ package grp.meca.irpf.Pojos;
 
 import java.util.List;
 
+import grp.meca.irpf.Models.EventoExtraordinario;
 import grp.meca.irpf.Models.NotaDeCorretagem;
 import grp.meca.irpf.Services.DayTradeService;
 import grp.meca.irpf.Services.SwingTradeService;
@@ -13,10 +14,10 @@ public class Relatorio {
 	private DayTradeService dtService;
 	private SwingTradeService stService;
 	
-	public Relatorio(List<NotaDeCorretagem> corretagens) {
+	public Relatorio(List<NotaDeCorretagem> corretagens, List<EventoExtraordinario> eventos) {
 		dtService = new DayTradeService();
-		dtService.calcularDadosDoTrade(corretagens);
+		dtService.calcularDadosDoTrade(corretagens, null);
 		stService = new SwingTradeService();
-		stService.calcularDadosDoTrade(corretagens);
+		stService.calcularDadosDoTrade(corretagens, eventos);
 	}
 }
