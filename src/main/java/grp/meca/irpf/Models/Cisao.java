27a48de250638ea86023e424c4f9bb03a6afd27f
plojr <ufvpaulo@gmail.com>
2022-06-28 20:43:26 -0300

    Implementação do método aplicarEvento()

--- src/main/java/grp/meca/irpf/Models/Cisao.java
+++ src/main/java/grp/meca/irpf/Models/Cisao.java
@@ -14,11 +14,16 @@
 
 package grp.meca.irpf.Models;
 
+import java.time.LocalDate;
+import java.util.Map;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
+
+import org.springframework.data.util.Pair;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -28,14 +33,45 @@ import lombok.EqualsAndHashCode;
 @Entity(name = "cisao")
 public class Cisao extends EventoExtraordinario {
 	
+	public Cisao() {}
+	
+	public Cisao(Ticker ticker1, LocalDate dataEvento, Ticker ticker2, double parteCindida, double proporcaoDeAcoes) {
+		super(ticker1, dataEvento);
+		this.ticker2 = ticker2;
+		this.parteCindida = parteCindida;
+		this.proporcaoDeAcoes = proporcaoDeAcoes;
+	}
+
 	// Esta é a empresa que foi cindida.
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "ticker_id_2", nullable = false)
 	private Ticker ticker2;
 	
+	/*
+	 * Parte cindida é o decimal equivalente à porcentagem que será cindida da empresa original.
+	 */
 	@Column(nullable = false)
-	private double porcentagemCindida;
+	private double parteCindida;
 	
+	/*
+	 * Só para ficar claro: proporção de ações significa quantas ações da empresa cindida o investidor vai receber 
+	 * para cada ação da empresa original, e não quantas ações da empresa original o investidor precisa ter para 
+	 * receber 1 da empresa cindida.
+	 * No exemplo da cisão da Getnet, a proporção foi de 0,25, isto é, para cada ação de Santander, o investidor
+	 * recebeu 0,25 ações da Getnet.
+	 */
 	@Column(nullable = false)
-	private double proporcao;
+	private double proporcaoDeAcoes;
+	
+	public void aplicarEvento(Map<String, Pair<Integer, Double>> carteira) {
+		if(carteira == null) return;
+		String tickerEmpresaOriginal = getTicker1().getCodigo(), tickerEmpresaCindida = getTicker2().getCodigo();
+		if(!carteira.containsKey(tickerEmpresaOriginal)) return;
+		int quantidadeEmpresaOriginal = carteira.get(tickerEmpresaOriginal).getFirst();
+		double custoEmpresaOriginal = carteira.get(tickerEmpresaOriginal).getSecond();
+		int quantidadeEmpresaCindida =  (int)(this.proporcaoDeAcoes*quantidadeEmpresaOriginal);
+		double custoEmpresaCindida = this.parteCindida*custoEmpresaOriginal;
+		carteira.put(tickerEmpresaCindida, Pair.of(quantidadeEmpresaCindida, custoEmpresaCindida));
+		carteira.put(tickerEmpresaOriginal, Pair.of(quantidadeEmpresaOriginal, custoEmpresaOriginal-custoEmpresaCindida));
+	}
 }
